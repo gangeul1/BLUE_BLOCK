04b1d037c61d143c1fa854d7071819a8e77fc69c
gangeul1 <schooleul@gmail.com>
2023-09-03 19:01:48 +0900

    Revert "wasd 호환 가능"
    This reverts commit 29ff06c22fa2376384395318a9ee067176d2910a.

--- main.py
+++ main.py
@@ -177,10 +177,8 @@ def dead():
 def restart():
     global dt , running , Jump , Dash , PlayerXto, jump_power , \
         game_start_count , xspeed, jump_power_set, able_jump,\
-        re_block_count, re_enemy_count, right_pressed, left_pressed
+        re_block_count, re_enemy_count
     if game_start_count < game_over_count:
-        right_pressed = False
-        left_pressed =  False
         PlayerXto = 0
         xspeed = 0
         player.x_lot = 325
@@ -209,14 +207,6 @@ def restart():
                     re_enemy_count += 1
                     globals()[f"enemy{re_enemy_count}"].x_lot = -500 + char * 50
                     globals()[f"enemy{re_enemy_count}"].y_lot = line * 50
-def x_move(right_pressed, left_pressed):
-    global PlayerXto
-    if right_pressed == left_pressed:
-        PlayerXto = 0
-    elif right_pressed == True:
-        PlayerXto = 1
-    else:
-        PlayerXto = -1
 
 
 #################################################################################################   
@@ -272,29 +262,28 @@ def main():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP or event.key == pygame.K_w:
+            if event.key == pygame.K_UP:
                 Jump = True
-            if event.key == pygame.K_DOWN or event.key == pygame.K_s:
+            if event.key == pygame.K_DOWN:
                 Dash = True
-            if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
+            if event.key == pygame.K_RIGHT:
                 right_pressed = True
-            if event.key == pygame.K_LEFT or event.key == pygame.K_a:
+                PlayerXto += 1
+            if event.key == pygame.K_LEFT:
+                PlayerXto -= 1
                 left_pressed = True
 
         if event.type == pygame.KEYUP:
-            if event.key == pygame.K_UP or event.key == pygame.K_w:
+            if event.key == pygame.K_UP:
                 Jump = False
-            if event.key == pygame.K_DOWN or event.key == pygame.K_s:
+            if event.key == pygame.K_DOWN:
                 Dash = False
-            if event.key == pygame.K_RIGHT and right_pressed == True\
-                or event.key == pygame.K_d and right_pressed == True:
+            if event.key == pygame.K_RIGHT and right_pressed == True:
+                PlayerXto -= 1
                 right_pressed = False
-            if event.key == pygame.K_LEFT and left_pressed == True\
-                or event.key == pygame.K_a and left_pressed == True:
+            if event.key == pygame.K_LEFT and left_pressed == True:
+                PlayerXto += 1
                 left_pressed = False
-        if right_pressed == True:
-            PlayerXto += 1
-        x_move(right_pressed, left_pressed)
 
 
     for i in range(1,block_count+1):
